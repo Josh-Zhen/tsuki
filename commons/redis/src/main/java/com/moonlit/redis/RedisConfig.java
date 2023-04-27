ed34ea93771388092593338a97ba3798d664cf74
Joshua <by.Moonlit@gmail.com>
2023-04-27 22:02:54 +0800

    [update] 更新Redis相關配置

---
+++ commons/redis/src/main/java/com/moonlit/redis/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.moonlit.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @author Joshua
+ * @version 1.0
+ * @date 27/4/2023 21:30
+ * @email by.Moonlit@hotmail.com
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+}
